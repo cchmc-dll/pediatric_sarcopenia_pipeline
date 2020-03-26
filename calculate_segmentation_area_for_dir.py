9ee3e4abd99fdd83d0a433615cf6d9422ebef328
Jimmy Castiglione <JamesCastiglione241@gmail.com>
2020-03-26 09:49:37 -0400

    Adds programs to calculate segmentation area
    The calculate_segmentation_area_for_dir.py script will calculate the
    segmentation area for the subjects in a directory. It requires the path
    to a directory with the original dicom images (in Tim's output format)
    to get the spacing and resolution of the original images. It prints the
    output in csv format.
    
    The calculate_seg_areas_for_dirs.py script will accept a list of dirs
    and call the previous script on each of the directories, outputing the
    results to multiple csv files (for now, try excel).

---
+++ calculate_segmentation_area_for_dir.py
@@ -0,0 +1,108 @@
+from argparse import ArgumentParser
+from collections import namedtuple
+import glob
+import itertools
+from pathlib import Path
+import sys
+
+import imageio
+import numpy as np
+import toolz
+
+from L3_finder.ingest import find_subjects, find_series
+
+
+def main(argv):
+    """sma is skeletal muscle area, sum of mask pixels * pixel area"""
+    args = parse_args(argv)
+    smas = calculate_sma(args)
+
+    print("subject_id,area_mm2")
+    for subject_id, area_mm2 in smas:
+        print("{},{}".format(subject_id, area_mm2))
+
+
+def parse_args(argv):
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        '--dicom_dir',
+        required=True,
+        help='Root directory containing dicoms in format output by Tim\'s '
+             'script. That is subject_1/accession_xyz/series{sagittal & '
+             'axial}. The accession directory should contain both a sagittal '
+             'image series and an axial image series. '
+    )
+
+    parser.add_argument(
+        '--segmentation_dir',
+        required=True,
+        help="Directory containing the segmentations in format"
+             "[STUDY_ID]/[--prediction_image_name]"
+    )
+
+    parser.add_argument(
+        '--segmentation_image_name',
+        required=True,
+        help='Name of the segmentation image, include extension'
+             'ex: predition.tif'
+    )
+
+    return parser.parse_args(argv)
+
+
+def calculate_sma(args):
+    axial_series = _find_axial_series(args.dicom_dir)
+    segmentations = _find_segmentation_images(
+        args.segmentation_dir, args.segmentation_image_name
+    )
+    return _calculate_smas(axial_series, segmentations)
+
+
+def _find_axial_series(dicom_dir):
+    subjects = find_subjects(dicom_dir)
+    all_series = _flatten(map(find_series, subjects))
+    return list(filter(lambda s: s.orientation == 'axial', all_series))
+
+
+def _flatten(sequence):
+    return itertools.chain(*sequence)
+
+
+SegmentationImage = namedtuple("SegmentationImage", ["subject_id", "image_data"])
+
+
+def _find_segmentation_images(segmentation_dir, image_name):
+    for image_path in glob.glob("{}/**/{}".format(segmentation_dir, image_name)):
+        yield SegmentationImage(
+            subject_id=_subject_id_from_seg_image_path(image_path),
+            image_data=imageio.imread(image_path),
+        )
+
+
+def _subject_id_from_seg_image_path(image_path):
+    return Path(image_path).parent.name
+
+
+SegmentationArea = namedtuple("SegmentationArea", ["subject_id", "area_mm2"])
+
+
+def _calculate_smas(axial_series, segmentations):
+    series_segmentation_pairs = toolz.join(
+        leftkey=lambda ax: ax.subject.id_,
+        leftseq=axial_series,
+        rightkey=lambda s: s.subject_id,
+        rightseq=segmentations
+    )
+
+    for series, segmentation in series_segmentation_pairs:
+        scale_factor = np.product(np.array(series.resolution) / np.array(segmentation.image_data.shape))
+        segmented_pixels = np.count_nonzero(segmentation.image_data)
+        pixel_area = np.product(series.true_spacing)
+        yield SegmentationArea(
+            subject_id=series.subject.id_,
+            area_mm2=pixel_area * segmented_pixels * scale_factor
+        )
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
